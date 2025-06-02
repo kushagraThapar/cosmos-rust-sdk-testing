b8ade770730e839b3e2c30119decb2aaa5146e8e
Kushagra Thapar <kuthapar@microsoft.com>
2025-06-02 15:12:14 -0700

    Added testing file to print hello world

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/hello_world/target/debug/hello_world: /Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/hello_world/src/main.rs
