74ef8eace89f78f635fd0f005963825a8dbc0bc1
Kushagra Thapar <kuthapar@microsoft.com>
2025-06-10 15:11:39 -0700

    Added new cargo crate for variables and mutability

---
+++ variables_mutability/target/debug/deps/variables_mutability-1b64d2f3001bc468.d
@@ -0,0 +1,5 @@
+/Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/variables_mutability/target/debug/deps/libvariables_mutability-1b64d2f3001bc468.rmeta: src/main.rs
+
+/Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/variables_mutability/target/debug/deps/variables_mutability-1b64d2f3001bc468.d: src/main.rs
+
+src/main.rs:
---
+++ variables_mutability/target/debug/deps/variables_mutability-5c8222ddbe29d9cd.d
@@ -0,0 +1,5 @@
+/Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/variables_mutability/target/debug/deps/libvariables_mutability-5c8222ddbe29d9cd.rmeta: src/main.rs
+
+/Users/kushagrathapar/Data/microsoft/sandbox/cosmos-rust-sdk-testing/variables_mutability/target/debug/deps/variables_mutability-5c8222ddbe29d9cd.d: src/main.rs
+
+src/main.rs:
